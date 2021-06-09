3884e572a470d93ccc6de4595d7a42faf84ed05e
tobiasbu <flamenco.bluegrass@gmail.com>
2021-06-09 11:28:24 -0300

    Engine: Create Exceptions file

---
+++ RhythmosEngine/RhythmosEngine/Exceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RhythmosEngine
+{
+    public class DatabaseLoadException : Exception
+    {
+        public DatabaseLoadException(string message)
+            : base(message) 
+        { }
+        public DatabaseLoadException(string message, Exception innerException)
+            : base(string.Concat("RhythmosEngine: ", message), innerException)
+        { }
+
+    }
+
+}
