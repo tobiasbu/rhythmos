4973870443861677dbfbec7c595fce4779fbcce9
tobiasbu <flamenco.bluegrass@gmail.com>
2021-06-09 11:30:15 -0300

    Engine: Change RhythmosDatabase
    - Create Load static function

--- RhythmosEngine/RhythmosEngine/RhythmosDatabase.cs
+++ RhythmosEngine/RhythmosEngine/RhythmosDatabase.cs
@@ -17,21 +17,21 @@ using System.IO;
 namespace RhythmosEngine
 {
     /// <summary>
-    /// RhythmDatabase Class. Provides a data storage for rhythms and note 
-    /// layouts created from the Rhythmos Editor to use in your game.
+    /// Provides a data storage for rhythms and note 
+    /// layouts created from the RhythmosEditor to use in your game.
     /// </summary>
     [Serializable]
     public class RhythmosDatabase
     {
 
         [SerializeField]
-        private List<Rhythm> m_rhythmDatabase = new List<Rhythm>();
+        private List<Rhythm> m_rhythmDatabase;
 
         [SerializeField]
-        private List<NoteLayout> m_noteLayoutList = new List<NoteLayout>();
+        private List<NoteLayout> m_noteLayoutList;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="RhythmosEngine.RhythmosDatabase"/> class.
+        /// Initializes a new instance of the <see cref="RhythmosEngine.RhythmosDatabase"/>.
         /// </summary>
         public RhythmosDatabase()
         {
@@ -43,6 +43,7 @@ namespace RhythmosEngine
         /// Initializes a new instance of the <see cref="RhythmosEngine.RhythmosDatabase"/> class.
         /// </summary>
         /// <param name="textAsset">Text asset.</param>
+        [Obsolete("This constructor is deprecated and is unsafe. Please use 'Load' method instead.", true)]
         public RhythmosDatabase(TextAsset textAsset)
         {
             m_rhythmDatabase = new List<Rhythm>();
@@ -98,10 +99,10 @@ namespace RhythmosEngine
         /// </summary>
         /// <returns><c>true</c>, if rhythmos database was loaded, <c>false</c> otherwise.</returns>
         /// <param name="resourcePath">Resource path.</param>
+        [Obsolete("'LoadRhythmosDatabase' is deprecated and will removed in next version. Please use 'Load' method instead.", true)]
         public bool LoadRhythmosDatabase(string resourcePath)
         {
             TextAsset textAsset = (TextAsset)Resources.Load(resourcePath, typeof(TextAsset));
-
             if (textAsset != null)
             {
                 LoadRhythmosDatabase(textAsset);
@@ -120,6 +121,7 @@ namespace RhythmosEngine
         /// </summary>
         /// <returns><c>true</c>, if rhythmos database was loaded, <c>false</c> otherwise.</returns>
         /// <param name="textAsset">Text asset.</param>
+        [Obsolete("'LoadRhythmosDatabase' is deprecated and will removed in next version. Please use 'Load' method instead.", true)]
         public bool LoadRhythmosDatabase(TextAsset textAsset)
         {
             if (textAsset != null)
@@ -131,7 +133,6 @@ namespace RhythmosEngine
                 try
                 {
                     xmlDoc.Load(reader);
-
                     m_noteLayoutList.Clear();
                     m_rhythmDatabase.Clear();
 
@@ -240,15 +241,9 @@ namespace RhythmosEngine
 
                                 AddRhythm(rtm);
                             }
-
-
                         }
                     }
-
-
                     return true;
-
-
                 }
                 catch (Exception ex)
                 {
@@ -264,18 +259,180 @@ namespace RhythmosEngine
         }
 
         /// <summary>
-        /// Adds a rhythm.
+        /// Loads RhythmosDatabas by given resource path ("Assets/Resources/...").
         /// </summary>
-        /// <param name="newRhythm">New rhythm.</param>
+        /// <param name="resourcePath">Resource path</param>
+        /// <returns>Loaded RhythmosDatabse</returns>
+        /// <exception cref="DatabaseLoadException">Throws if could not load RhythmosDatabase file correctly</exception
+        public static RhythmosDatabase Load(string resourcePath)
+        {
+            TextAsset textAsset = (TextAsset)Resources.Load(resourcePath, typeof(TextAsset));
+            return Load(textAsset);
+        }
+
+        /// <summary>
+        /// Load RhythmosDatabase from a TextAsset (XML file).
+        /// </summary>
+        /// <param name="textAsset">Text asset</param>
+        /// <returns>Loaded RhythmosDatabse</returns>
+        /// <exception cref="DatabaseLoadException">Throws if could not load RhythmosDatabase file correctly</exception>
+        public static RhythmosDatabase Load(TextAsset textAsset)
+        {
+            return Load(textAsset, new ResourceAssetLoader());
+        }
+        /// <summary>
+        /// Load RhythmosDatabase from a TextAsset (XML file).
+        /// 
+        /// <para>
+        /// NOTE: The <paramref name="loader"/> parameter defines how the RhythmDatabase will load AudioClips.
+        /// In Unity to load an asset in runtime you need to use <see cref="Resources"/> utility. 
+        /// To work properly with RhythmosDatabase, make sure to put your assets inside "Resources" folder.
+        /// </para>
+        /// 
+        /// <para>
+        /// In the future we'll provide a better way to load the RhtythmosDatabase using <see cref="ScriptableObject" />.
+        /// </para>
+        /// 
+        /// </summary>
+        /// <param name="textAsset">Text asset</param>
+        /// <param name="loader">Implementation to load assets</param>
+        /// <returns>Loaded RhythmosDatabse</returns>
+        /// <exception cref="DatabaseLoadException">Throws if could not load RhythmosDatabase file correctly</exception>
+        public static RhythmosDatabase Load(TextAsset textAsset, IAssetLoader loader)
+        {
+            RhythmosDatabase database = null;
+
+            if (textAsset != null)
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                MemoryStream assetStream = new MemoryStream(textAsset.bytes);
+                XmlReader reader = XmlReader.Create(assetStream);
+
+                try
+                {
+                    xmlDoc.Load(reader);
+                    database = new RhythmosDatabase();
+
+                    XmlNodeList noteList = xmlDoc.GetElementsByTagName("NoteEntry");
+                    foreach (XmlNode node in noteList)
+                    {
+                        Dictionary<string, string> subDictionary = new Dictionary<string, string>();
+                        XmlNodeList noteContet = node.ChildNodes;
+
+                        foreach (XmlNode noteItens in noteContet)
+                        {
+                            if (noteItens.Name == "name" || noteItens.Name == "audioclip" || noteItens.Name == "color")
+                            {
+                                subDictionary.Add(noteItens.Name, noteItens.InnerText);
+                            }
+                        }
+
+                        if (subDictionary.Count == 3)
+                        {
+                            NoteLayout nt = new NoteLayout();
+                            foreach (KeyValuePair<string, string> entry in subDictionary)
+                            {
+
+                                if (entry.Key == "name")
+                                    nt.Name = entry.Value;
+                                else if (entry.Key == "color")
+                                    nt.Color = Utility.ParseColor(entry.Value);
+                                else if (entry.Key == "audioclip")
+                                {
+                                    if (entry.Value == "0")
+                                        nt.Clip = null;
+                                    else
+                                    {
+                                        string path = entry.Value;
+                                        nt.Clip = loader.LoadClip(path);
+                                    }
+                                }
+                            }
+                            database.AddNoteLayout(nt);
+                        }
+                    }
+
+                    XmlNodeList RhythmEntry = xmlDoc.GetElementsByTagName("RhythmEntry");
+                    foreach (XmlNode node in RhythmEntry)
+                    {
+                        Dictionary<string, string> subDictionary = new Dictionary<string, string>();
+                        XmlNodeList rhythmContent = node.ChildNodes;
+                        Rhythm rtm = new Rhythm();
+
+                        foreach (XmlNode rhythmItens in rhythmContent)
+                        {
+                            if (rhythmItens.Name == "name" || rhythmItens.Name == "bpm")
+                            {
+                                subDictionary.Add(rhythmItens.Name, rhythmItens.InnerText);
+                            }
+                            else if (rhythmItens.Name == "RhythmSequence")
+                            {
+                                XmlNodeList sequence = rhythmItens.SelectNodes("./Note");
+                                foreach (XmlNode nodeNote in sequence)
+                                {
+                                    XmlNodeList childSequence = nodeNote.ChildNodes;
+                                    Dictionary<string, string> dictionarySequence = new Dictionary<string, string>();
+
+                                    foreach (XmlNode nodeNoteItens in childSequence)
+                                    {
+                                        if (nodeNoteItens.Name == "rest" || nodeNoteItens.Name == "duration" || nodeNoteItens.Name == "layoutIndex")
+                                        {
+                                            dictionarySequence.Add(nodeNoteItens.Name, nodeNoteItens.InnerText);
+                                        }
+                                    }
+
+                                    Note nt = new Note();
+                                    foreach (KeyValuePair<string, string> entry in dictionarySequence)
+                                    {
+                                        if (entry.Key == "duration")
+                                            nt.duration = float.Parse(entry.Value);
+                                        else if (entry.Key == "rest")
+                                            nt.isRest = bool.Parse(entry.Value);
+                                        else if (entry.Key == "layoutIndex")
+                                            nt.layoutIndex = int.Parse(entry.Value);
+                                    }
+                                    rtm.AppendNote(nt);
+                                }
+
+                                foreach (KeyValuePair<string, string> entry in subDictionary)
+                                {
+                                    if (entry.Key == "name")
+                                        rtm.Name = entry.Value;
+                                    else if (entry.Key == "bpm")
+                                        rtm.BPM = float.Parse(entry.Value);
+                                }
+
+                                database.AddRhythm(rtm);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new DatabaseLoadException(" Error loading the TextAsset: " + textAsset.name, ex);   
+                }
+            }
+            else
+            {
+                throw new DatabaseLoadException("TextAsset is null. Impossible to load RhythmosDatabase file."); 
+            }
+
+            return database;
+        }
+
+        /// <summary>
+        /// Adds a rhythm
+        /// </summary>
+        /// <param name="newRhythm">New rhythm</param>
         public void AddRhythm(Rhythm newRhythm)
         {
             m_rhythmDatabase.Add(newRhythm);
         }
 
         /// <summary>
-        /// Adds a note layout.
+        /// Adds a note layout
         /// </summary>
-        /// <param name="newNoteLayout">New note layout.</param>
+        /// <param name="newNoteLayout">New note layout</param>
         public void AddNoteLayout(NoteLayout newNoteLayout)
         {
             m_noteLayoutList.Add(newNoteLayout);
@@ -284,14 +441,14 @@ namespace RhythmosEngine
         /// <summary>
         /// Removes a rhythm at index.
         /// </summary>
-        /// <param name="index">Index.</param>
+        /// <param name="index">Index</param>
         public void RemoveRhythm(int index)
         {
             m_rhythmDatabase.RemoveAt(index);
         }
 
         /// <summary>
-        /// Removes a note layout at index.
+        /// Removes a note layout at index
         /// </summary>
         /// <param name="index">Index.</param>
         public void RemoveNoteLayout(int index)
@@ -300,7 +457,7 @@ namespace RhythmosEngine
         }
 
         /// <summary>
-        /// Clears the rhythm list.
+        /// Clears the rhythm list
         /// </summary>
         public void ClearRhythmList()
         {
@@ -308,7 +465,7 @@ namespace RhythmosEngine
         }
 
         /// <summary>
-        /// Clears the note layout list.
+        /// Clears the note layout list
         /// </summary>
         public void ClearNoteLayoutList()
         {
