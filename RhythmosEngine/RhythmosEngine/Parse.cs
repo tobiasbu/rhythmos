380c2947544ccf2b2761d9d2b49dcc4abe211d60
tobiasbu <flamenco.bluegrass@gmail.com>
2021-06-09 17:50:54 -0300

    Engine: Fix string parse
    - Due to cultural differences, we need to use
    CultureInfo.InvariantCulture to format or parse a string that
    does not considers user's local settings.
    - Fix typos and small sentences
    - Remove Utility class

---
+++ RhythmosEngine/RhythmosEngine/Parse.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using UnityEngine;
+
+namespace RhythmosEngine
+{
+    internal static class Parse
+    { 
+
+        public static float Float(string value)
+        {
+            return float.Parse(value, CultureInfo.InvariantCulture);
+        }
+
+        public static Color Color(string col)
+        {
+            Color output = new Color(0, 0, 0, 0);
+            if (col.StartsWith("RGBA("))
+            {
+                col = col.Remove(0, 5);
+                col = col.Remove(col.Length - 1, 1);
+            }
+            string[] strs = col.Split(","[0]);
+
+            for (int i = 0; i < strs.Length; i++)
+            {
+                float value = Float(strs[i]);
+                output[i] = Mathf.Clamp(value, 0.0f, 1.0f);
+            }
+            return output;
+        }
+    }
+}
