29dd69831530e79ec7bbe3a5d4cee4908488d517
tobiasbu <flamenco.bluegrass@gmail.com>
2021-06-09 11:28:02 -0300

    Engine: Create IAssetLoader
    - Related to load improvement for RhythmosEditor

---
+++ RhythmosEngine/RhythmosEngine/IAssetLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace RhythmosEngine
+{
+    /// <summary>
+    /// Provides a way to load assets.
+    /// </summary>
+    public interface IAssetLoader
+    {
+        /// <summary>
+        /// Load <c>AudioClip</c> by given path.
+        /// </summary>
+        /// <param name="path">Path to <c>AudioClip</c> asset</param>
+        /// <returns>AudioClip asset</returns>
+        AudioClip LoadClip(string path);
+    }
+}
---
+++ RhythmosEngine/RhythmosEngine/ResourceAssetLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace RhythmosEngine
+{
+    internal class ResourceAssetLoader : IAssetLoader
+    {
+        public AudioClip LoadClip(string path)
+        {
+            string runtimePath = path.Replace("Assets/Resources/", "");
+            runtimePath = runtimePath.Remove(runtimePath.Length - 4, 4);
+            return (AudioClip)Resources.Load(runtimePath, typeof(AudioClip));
+        }
+    }
+}
