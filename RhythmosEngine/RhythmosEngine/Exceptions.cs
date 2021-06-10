db6bfddcdf743a3212362372afcd0e3742e750ac
tobiasbu <flamenco.bluegrass@gmail.com>
2021-06-10 20:08:31 -0300

    Engine: Write Exceptions documentation

--- RhythmosEngine/RhythmosEngine/Exceptions.cs
+++ RhythmosEngine/RhythmosEngine/Exceptions.cs
@@ -2,11 +2,24 @@
 
 namespace RhythmosEngine
 {
+    /// <summary>
+    /// Represents an exception for database loader <see cref="RhythmosEngine.RhythmosDatabase"/>
+    /// </summary>
     public class DatabaseLoadException : Exception
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="message"></param>
         public DatabaseLoadException(string message)
-            : base(message) 
+            : base(string.Concat("RhythmosEngine: ", message)) 
         { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
         public DatabaseLoadException(string message, Exception innerException)
             : base(string.Concat("RhythmosEngine: ", message), innerException)
         { }
