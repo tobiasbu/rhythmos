47b7ebbfbc03b97fb6d24d2e8247726fc2d043ed
tobiasbu <flamenco.bluegrass@gmail.com>
2021-06-11 20:20:01 -0300

    Editor: Create EditorAssetLoader

---
+++ RhythmosEditor/RhythmosEditor/EditorAssetLoader.cs
@@ -0,0 +1,14 @@
+using RhythmosEngine;
+using UnityEditor;
+using UnityEngine;
+
+namespace RhythmosEditor
+{
+    internal class EditorAssetLoader : IAssetLoader
+    {
+        public AudioClip LoadClip(string path)
+        {
+           return (AudioClip)AssetDatabase.LoadAssetAtPath(path, typeof(AudioClip));
+        }
+    }
+}
