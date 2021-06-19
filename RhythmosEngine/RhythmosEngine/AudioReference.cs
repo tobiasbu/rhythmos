2f8e4c6bbd2f8261cce3b0509a00f90102c41f53
tobiasbu <flamenco.bluegrass@gmail.com>
2021-06-19 13:34:16 -0300

    Engine: Deprecate NoteLayout
    - Create AudioReference

---
+++ RhythmosEngine/RhythmosEngine/AudioReference.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace RhythmosEngine
+{
+    /// <summary>
+    /// Stores <see cref="AudioClip"/> reference and related <see cref="UnityEngine.Color"/> identifier
+    /// </summary>
+    [Serializable]
+    public class AudioReference 
+    {
+        /// <summary>
+        /// Actual <see cref="AudioClip"/> reference
+        /// </summary>
+        public AudioClip Clip;
+
+        /// <summary>
+        /// Color identifier
+        /// </summary>
+        public Color Color;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RhythmosEngine.AudioReference"/>
+        /// </summary>
+        public AudioReference()
+        {
+            Clip = null;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RhythmosEngine.AudioReference"/>
+        /// </summary>
+        /// <param name="clip">AudioClip</param>
+        public AudioReference(AudioClip clip)
+        {
+            Clip = clip;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RhythmosEngine.AudioReference"/>
+        /// </summary>
+        /// <param name="clip">AudioClip</param>
+        /// <param name="color">Color identifier</param>
+        public AudioReference(AudioClip clip, Color color)
+        {
+            Clip = clip;
+            Color = color;
+        }
+
+        /// <summary>
+        /// Copy constructor
+        /// </summary>
+        /// <param name="otherAudioReference">Other <see cref="RhythmosEngine.AudioReference"/> instance to copy</param>
+        public AudioReference(AudioReference otherAudioReference) {
+            Color = otherAudioReference.Color;
+            Clip = otherAudioReference.Clip;
+        }
+    }
+}
