2aa477aa3f9301dbb1ccf94f01473302c46c5023
tobiasbu <flamenco.bluegrass@gmail.com>
2021-06-19 13:05:48 -0300

    Editor: Create DragDrop GUI utility class

---
+++ RhythmosEditor/RhythmosEditor/GUI/DragDrop.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace RhythmosEditor
+{
+    internal static class DragDrop<T>
+    {
+        private static object[] dragDropItems = null;
+
+        public static bool Valid { private set; get; } = false;
+
+        public static bool HasValidType(object[] objectReferences, Type type)
+        {
+            int counter = 0;
+            foreach (var value in objectReferences)
+            {
+                Type currentType = value.GetType();
+                if (currentType.Equals(type))
+                {
+                    counter += 1;
+                }
+            }
+
+            return counter != 0;
+        }
+
+        public static bool HasValidType(object[] objectReferences)
+        {
+            Type type = typeof(T);
+            return HasValidType(objectReferences, type);
+        }
+
+        private static T[] Filter()
+        {
+            if (dragDropItems == null)
+            {
+                return null;
+            }
+
+            List<T> filteredItems = new List<T>();
+            Type type = typeof(T);
+            foreach (var value in dragDropItems)
+            {
+                Type currentType = value.GetType();
+                if (currentType.Equals(type))
+                {
+                    filteredItems.Add((T)value);
+                }
+            }
+            return filteredItems.ToArray();
+        }
+
+        public static T[] Do(Event currentEvent)
+        {
+            if (currentEvent != null)
+            {
+                switch (currentEvent.type)
+                {
+                    case EventType.MouseUp: // Clean up, in case MouseDrag never occurred:
+                    case EventType.DragExited:
+                        dragDropItems = null;
+                        Valid = false;
+                        DragAndDrop.PrepareStartDrag();
+                        break;
+
+                    case EventType.DragUpdated:
+                        if (HasValidType(DragAndDrop.objectReferences))
+                        {
+                            Valid = true;
+                            dragDropItems = DragAndDrop.objectReferences;
+                            DragAndDrop.visualMode = DragAndDropVisualMode.Move;
+                        }
+                        else
+                        {
+                            Valid = false;
+                            dragDropItems = null;
+                            DragAndDrop.visualMode = DragAndDropVisualMode.Rejected;
+                        }
+                        break;
+
+                    case EventType.DragPerform:
+                        return Filter();
+                }
+            }
+            return null;
+        }
+    }
+
+
+}
