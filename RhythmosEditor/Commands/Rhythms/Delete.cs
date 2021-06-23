15d5fd6b14c2788faaa2389407afc9528081e6ce
tobiasbu <flamenco.bluegrass@gmail.com>
2021-06-22 22:39:22 -0300

    Editor: Create rhythm management commands

---
+++ RhythmosEditor/Commands/Rhythms/Delete.cs
@@ -0,0 +1,26 @@
+using RhythmosEngine;
+
+namespace RhythmosEditor.Commands.Rhythms
+{
+    using RhythmListView = ListView<Rhythm>;
+
+    internal class Delete : BaseCommand
+    {
+        public override string Name => "Remove " + (rhythm != null ? rhythm.Name : "Rhythm");
+
+        public Delete(RhythmListView list)
+            : base(list, list.SelectedIndex, list.Selected)
+        {
+        }
+
+        public override void Execute()
+        {
+            Commons.RemoveRhythmAt(rhythmListView, rhythm, index);
+        }
+
+        public override void UnExecute()
+        {
+            Commons.AddRhythmAt(rhythmListView, rhythm, index);
+        }
+    }
+}
---
+++ RhythmosEditor/Commands/Rhythms/Duplicate.cs
@@ -0,0 +1,39 @@
+using RhythmosEngine;
+
+namespace RhythmosEditor.Commands.Rhythms
+{
+    using RhythmListView = ListView<Rhythm>;
+
+    internal class Duplicate : BaseCommand
+    {
+        public override string Name => "Duplicate " + (rhythm != null ? rhythm.Name : "Rhythm");
+
+        public Duplicate(RhythmListView list)
+            : base(list, -1, null)
+        { }
+
+        public override void Execute()
+        {
+            if (index == -1)
+            {
+                Rhythm clone = new Rhythm(rhythmListView.Selected);
+                clone.Name = string.Concat(clone.Name, " (Copy)");
+
+                int insertIndex = rhythmListView.SelectedIndex + 1;
+                Commons.AddRhythmAt(rhythmListView, clone, insertIndex);
+
+                index = insertIndex;
+                rhythm = clone;
+            }
+            else
+            {
+                Commons.AddRhythmAt(rhythmListView, rhythm, index);
+            }
+        }
+
+        public override void UnExecute()
+        {
+            Commons.RemoveRhythmAt(rhythmListView, rhythm, index);
+        }
+    }
+}
