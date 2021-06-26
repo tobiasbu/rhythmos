58d6b122b5dcdc7400f8a75ff2f142571f34a2ab
tobiasbu <flamenco.bluegrass@gmail.com>
2021-06-25 22:52:23 -0300

    Editor: Move exceptions to separate files

---
+++ RhythmosEditor/Exceptions/ExportXmlException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RhythmosEditor.Exceptions
+{
+    public class ExportXmlException : Exception
+    {
+        public ExportXmlException(string message)
+            : base(string.Concat("RhythmosEditor: ", message))
+        { }
+        public ExportXmlException(string message, Exception innerException)
+            : base(string.Concat("RhythmosEditor: ", message), innerException)
+        { }
+
+    }
+}
---
+++ RhythmosEditor/Exceptions/ImportXmlException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RhythmosEditor.Exceptions
+{
+    public class ImportXmlException : Exception
+    {
+        public ImportXmlException(string message)
+            : base(string.Concat("RhythmosEditor: ", message))
+        { }
+        public ImportXmlException(string message, Exception innerException)
+            : base(string.Concat("RhythmosEditor: ", message), innerException)
+        { }
+
+    }
+}
