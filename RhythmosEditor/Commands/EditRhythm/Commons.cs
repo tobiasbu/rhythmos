6335d464ff95a00e7081ee9f2c2f057e1180165b
tobiasbu <flamenco.bluegrass@gmail.com>
2021-07-01 20:24:36 -0300

    Editor: Create EditRhythm commands
    - Create BaseCommand
    - Create AddNote command
    - Create Rename command
    - Create ChangeBPM command
    - Create Commons class

---
+++ RhythmosEditor/Commands/EditRhythm/Commons.cs
@@ -0,0 +1,47 @@
+﻿using RhythmosEditor.Pages.Rhythms;
+using RhythmosEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RhythmosEditor.Commands.EditRhythm
+{
+    internal static class Commons
+    {
+        public static int AddNoteAt(EditController controller, int rhythmIndex, Note note, int noteIndex)
+        {
+            Rhythm rhythm = controller.GetRhythmAt(rhythmIndex);
+            int insertAt = Mathf.Clamp(noteIndex, 0, rhythm.Count);
+
+            if (rhythm.Count > 0)
+            {
+                rhythm.InsertNoteAt(insertAt, note);
+            }
+            else
+            {
+                rhythm.AppendNote(note);
+            }
+            controller.SelectNote(insertAt, rhythmIndex);
+            return insertAt;
+        }
+
+        internal static void RemoveNoteAt(EditController controller, int rhythmIndex, int noteIndex)
+        {
+            Rhythm rhythm = controller.GetRhythmAt(rhythmIndex);
+            rhythm.RemoveNote(noteIndex);
+
+            if (rhythm.Count > 0)
+            {
+                int selectAt = Mathf.Clamp(noteIndex, 0, rhythm.Count - 1);
+                controller.SelectNote(selectAt, rhythmIndex);
+            }
+            else
+            {
+                controller.UnSelectNote();
+            }
+        }
+    }
+}
