3f05fc87b53140a0606554d2891f9e2a8853ecd0
Tobias Ulrich <tobiasbulrich@gmail.com>
2021-07-06 21:01:30 -0300

    Editor: Create note commands

---
+++ RhythmosEditor/Commands/EditRhythm/DuplicateNote.cs
@@ -0,0 +1,33 @@
+﻿using RhythmosEditor.Pages.Rhythms;
+using RhythmosEngine;
+
+namespace RhythmosEditor.Commands.EditRhythm
+{
+    internal class DuplicateNote : BaseNoteCommand
+    {
+        public DuplicateNote(EditController controller) : base(controller)
+        {
+        }
+
+        public override string Name => "Duplicate note";
+
+        public override void Execute()
+        {
+            if (noteIndex == -1)
+            {
+                Note clone = new Note(controller.SelectedNote);
+                int insertIndex = controller.SelectedNoteIndex + 1;
+               
+                noteIndex = insertIndex;
+                note = clone;
+            }
+
+            Commons.AddNoteAt(controller, rhyhtmIndex, note, noteIndex);
+        }
+
+        public override void UnExecute()
+        {
+            Commons.RemoveNoteAt(controller, rhyhtmIndex, noteIndex);
+        }
+    }
+}
