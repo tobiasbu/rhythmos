086671ae252d3ff4db8d7ec1b20e8af24cd02c21
Tobias Ulrich <tobiasbulrich@gmail.com>
2021-07-10 11:05:38 -0300

    Editor: Create GUIUtils

--- RhythmosEditor/UI/GUIUtils.cs
+++ RhythmosEditor/UI/GUIUtils.cs
@@ -1,11 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace RhythmosEditor.UI
 {
     public static class GUIUtils
     {
+        private static Color oldColor;
+
         public static void SetColor(Color color)
         {
+            oldColor = GUI.color;
+
             if (GUI.enabled)
             {
                 GUI.color = color;
@@ -16,6 +21,45 @@ namespace RhythmosEditor.UI
                 GUI.color = color;
             }
 
+        }
+
+
+
+        public static Color GetColor(Color color, float replaceAlpha)
+        {
+            color.a = replaceAlpha;
+            if (!GUI.enabled) { 
+                color.a *= 0.675f; 
+            }
+            return color;
+        }
+
+        public static Color GetColor(Color color)
+        {
+            if (!GUI.enabled)
+            {
+                color.a *= 0.675f;
+            }
+            return color;
+        }
+
+        public static void ResetColor()
+        {
+            GUI.color = oldColor;
+        }
+
+        internal static void SetColor(Color enabledColor, Color disabledColor)
+        {
+            oldColor = GUI.color;
+
+            if (GUI.enabled)
+            {
+                GUI.color = enabledColor;
+            }
+            else
+            {
+                GUI.color = disabledColor;
+            }
         }
     }
 }
