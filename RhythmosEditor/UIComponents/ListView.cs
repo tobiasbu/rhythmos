0546702568b6e41c93bbec2985240501e6afa667
Tobias Ulrich <tobiasbulrich@gmail.com>
2021-07-13 21:13:13 -0300

    Editor: Improve ListVIew

--- RhythmosEditor/UIComponents/ListView.cs
+++ RhythmosEditor/UIComponents/ListView.cs
@@ -1,20 +1,17 @@
 using System;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
+using UnityEditor;
 using RhythmosEditor.UI;
 
 namespace RhythmosEditor.UIComponents
 {
     internal class ListView<T>
     {
-        // GUI items
-        private GUIStyle listItemStyle;
 
         // GUI variables
         private float barValue;
         private Rect currentBoxRect;
-        private Rect layoutRect;
 
         private IList<T> viewList;
         public IList<T> List {
@@ -26,16 +23,14 @@ namespace RhythmosEditor.UIComponents
                     UnSelect();
                 }
             }
-            get
-            {
+            get {
                 return viewList;
             }
         }
 
-        public float BarPosition
-        {
-            private set
-            {
+
+        public float BarPosition {
+            private set {
                 if (viewList != null)
                 {
                     barValue = value;
@@ -49,8 +44,7 @@ namespace RhythmosEditor.UIComponents
                     }
                 }
             }
-            get
-            {
+            get {
                 return barValue;
             }
         }
@@ -61,59 +55,69 @@ namespace RhythmosEditor.UIComponents
 
         public int SelectedIndex { private set; get; } = -1;
 
-        public float ItemHeight { set; get; } = 19;
+        public float ItemHeight { set; get; } = 20;
 
-        public bool HasSelection
-        {
-            get
-            {
+        public bool HasSelection {
+            get {
                 return List != null && List.Count != 0 && SelectedIndex >= 0;
             }
         }
 
+        public bool Focus { get; set; }
+        public bool KeyboardNavigation { get; internal set; } = true;
 
         public Func<T, string> onGetItemLabel;
         public Action<T, int> onSelectionChange;
-
-        private void Load()
-        {
-            if (listItemStyle == null)
-            {
-                listItemStyle = new GUIStyle("label");
-                GUIStyleState normalStyle = EditorStyles.label.normal;
-                listItemStyle.normal = normalStyle;
-                listItemStyle.normal.textColor = Color.white;
-                listItemStyle.contentOffset = new Vector2(2, -1);
-            }
-        }
+        internal Func<T, Rect, int, bool> onDrawItem;
 
         private void OnUpdate()
         {
             if (Event.current != null)
             {
                 Event current = Event.current;
+                Rect test = currentBoxRect;
+                test.y = 0;
 
-                if (currentBoxRect.Contains(current.mousePosition))
+                if (test.Contains(current.mousePosition))
                 {
                     float newBarPosition = BarPosition + GUIEvents.GetScrollDelta(current);
                     if (newBarPosition != BarPosition)
                     {
-                        BarPosition = newBarPosition;
+                        float max = 0;
+                        float totalSize = ItemHeight * (List != null ? List.Count : 0);
+                        if (totalSize > currentBoxRect.height)
+                        {
+                            max = (totalSize - currentBoxRect.height) / ItemHeight;
+                        }
+                        BarPosition = Mathf.Clamp(newBarPosition, 0, max);
                         Repainter.Request();
+                        current.Use();
                         return;
                     }
                 }
 
-                if (HasSelection && current.type == EventType.KeyDown)
+                if (Focus)
                 {
-                    if (current.keyCode == KeyCode.DownArrow)
+                    if (!currentBoxRect.Contains(current.mousePosition) && current.type == EventType.Ignore)
                     {
-                        MoveSelectionCursor(1);
-
+                        Focus = false;
+                        return;
                     }
-                    else if (current.keyCode == KeyCode.UpArrow)
+
+                    if (KeyboardNavigation && HasSelection && current.type == EventType.KeyDown)
                     {
-                        MoveSelectionCursor(-1);
+                        if (current.keyCode == KeyCode.DownArrow)
+                        {
+                            MoveSelectionCursor(1);
+                            current.Use();
+
+                        }
+                        else if (current.keyCode == KeyCode.UpArrow)
+                        {
+                            MoveSelectionCursor(-1);
+                            current.Use();
+                        }
+
                     }
                 }
             }
@@ -136,7 +140,6 @@ namespace RhythmosEditor.UIComponents
             if (changeScroll)
             {
                 BarPosition = itemIndex;
-                Repainter.Request();
             }
 
             if (itemIndex != SelectedIndex)
@@ -145,8 +148,9 @@ namespace RhythmosEditor.UIComponents
                 Current = List[itemIndex];
                 SelectedIndex = itemIndex;
                 onSelectionChange?.Invoke(Current, itemIndex);
-                Repainter.Request();
             }
+            Repainter.Request();
+
         }
 
         public void UnSelect()
@@ -176,82 +180,103 @@ namespace RhythmosEditor.UIComponents
                 return;
             }
 
-            int index = SelectedIndex + delta;
-            if (index == SelectedIndex)
+            int currentIndex = SelectedIndex;
+            int nextIndex = currentIndex + delta;
+            if (nextIndex == currentIndex)
             {
                 return;
             }
 
-            index = Mathf.Clamp(index, 0, List.Count - 1);
-            Select(index, false);
+            nextIndex = Mathf.Clamp(nextIndex, 0, List.Count - 1);
+            Select(nextIndex, false);
 
-            float itemY = index * ItemHeight;
+            float itemY = nextIndex * ItemHeight;
             float itemDelta = delta >= 1 ? ItemHeight : 0;
             float barY = BarPosition * ItemHeight;
+            float futurePos = itemY + itemDelta;
             Rect visibleRect = new Rect(0, barY, currentBoxRect.width, currentBoxRect.height);
             if (!visibleRect.Contains(new Vector2(0, itemY + itemDelta)))
             {
-                if (itemY + itemDelta > currentBoxRect.height)
+
+                if (futurePos > currentBoxRect.height)
                 {
-                    BarPosition = (itemY + itemDelta - currentBoxRect.height) / ItemHeight;
+                    if (delta > 0)
+                    {
+                        BarPosition = (futurePos - currentBoxRect.height) / ItemHeight;
+
+                    }
+                    else
+                    {
+                        BarPosition = futurePos / ItemHeight;
+                    }
+
                 }
                 else
                 {
-                    BarPosition = index;
+                    BarPosition = futurePos / ItemHeight;
                 }
             }
-
         }
 
-        private void OnDraw(Rect boxRect) {
-
-            Load();
-
+        private void OnDraw(Rect boxRect)
+        {
             string itemLabel = "";
             int count = List != null ? List.Count : 0;
             float totalsize = (ItemHeight * count);
             float y = -(ItemHeight * barValue);
-
-            Rect groupRect = boxRect;
-            Rect entryRect;
-
-            groupRect.width = boxRect.x + boxRect.width;
-            groupRect.x = 0;
-            entryRect = new Rect(boxRect.x, y, boxRect.width, ItemHeight);
-            if (totalsize > boxRect.height)
+            bool select;
+            Rect entryRect = new Rect(0, y, boxRect.width, ItemHeight);
+            if (totalsize >= boxRect.height)
             {
-                entryRect.width -= 14;
+                entryRect.width -= 15;
             }
 
-            GUI.BeginGroup(groupRect);
+            Color oldGuiColor = GUI.color;
+
+            GUI.BeginGroup(boxRect);
             for (int i = 0; i < count; i += 1)
             {
+                select = false;
                 entryRect.y = y;
-                if (onGetItemLabel != null)
-                {
-                    itemLabel = onGetItemLabel(List[i]);
-                }
+
 
                 if (SelectedIndex == i)
                 {
                     GUI.color = Colors.Selection;
                     GUI.DrawTexture(entryRect, Icons.Pixel);
-                    GUI.color = Color.white;
+                    GUI.color = oldGuiColor;
                 }
                 else
                 {
                     GUI.color = EditorStyles.label.normal.textColor;
                 }
 
-                GUI.Label(entryRect, itemLabel, listItemStyle);
-                GUI.color = Color.clear;
-                if (GUI.Button(entryRect, ""))
+                if (onDrawItem != null)
+                {
+
+                    GUILayout.BeginArea(entryRect);
+                    select |= onDrawItem(List[i], new Rect(0, 0, entryRect.width, entryRect.height), i);
+                    GUILayout.EndArea();
+
+                }
+                else
+                {
+
+                    itemLabel = onGetItemLabel?.Invoke(List[i]);
+                    GUI.Label(entryRect, itemLabel, Styles.ListLabel);
+                    GUI.color = Color.clear;
+                    select |= GUI.Button(entryRect, "");
+                }
+
+                if (select)
                 {
                     if (SelectedIndex != i)
                     {
                         Select(i, false);
+                        Focus = true;
                     }
                 }
+
                 y += ItemHeight;
             }
 
@@ -259,7 +284,7 @@ namespace RhythmosEditor.UIComponents
             if (totalsize >= boxRect.height)
             {
                 float viewableRatio = boxRect.height / totalsize;
-                barValue = GUI.VerticalScrollbar(new Rect(groupRect.width - 14, 0, 30, groupRect.height), barValue, count * viewableRatio, 0, count);
+                barValue = GUI.VerticalScrollbar(new Rect(boxRect.width - 14, 0, 30, boxRect.height), barValue, count * viewableRatio, 0, count);
             }
             else
             {
@@ -271,15 +296,9 @@ namespace RhythmosEditor.UIComponents
             GUI.EndGroup();
         }
 
-        public void Draw() {
-            layoutRect = EditorGUILayout.GetControlRect(GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
-            GUI.Box(layoutRect, "");
-            OnDraw(layoutRect);
-        }
-
-        public void Draw(float width, float height)
+        public void Draw()
         {
-           GUILayout.Box("", GUILayout.Width(width), GUILayout.Height(height));
+            GUILayout.Box("", GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
 
             // Unity issue:
             // See: https://issuetracker.unity3d.com/issues/guilayoututility-dot-getlastrect-returns-incorrect-rect-when-used-after-editorguilayout-dot-dropdownbutton
@@ -290,7 +309,21 @@ namespace RhythmosEditor.UIComponents
             }
 
             OnDraw(currentBoxRect);
+            // currentBoxRect = EditorGUILayout.GetControlRect(GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
+            // currentBoxRect = GUILayoutUtility.GetRect(100, 100, GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
+            // GUI.Box(currentBoxRect, "");
+        }
 
+        public void Draw(float width, float height)
+        {
+            GUILayout.Box("", GUILayout.Width(width), GUILayout.Height(height));
+
+            if (Event.current.type == EventType.Repaint)
+            {
+                currentBoxRect = GUILayoutUtility.GetLastRect();
+            }
+
+            OnDraw(currentBoxRect);
         }
 
     }
