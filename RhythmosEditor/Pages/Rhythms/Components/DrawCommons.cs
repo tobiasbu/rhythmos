c893dfb2da5c0e85daf85f7e4fd59b56a34ea41a
Tobias Ulrich <tobiasbulrich@gmail.com>
2021-07-18 21:01:31 -0300

    Editor: Improve DrawCommons and PlayerToolbar
    - Remove TrackDraw

---
+++ RhythmosEditor/Pages/Rhythms/Components/DrawCommons.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using RhythmosEditor.UI;
+
+namespace RhythmosEditor.Pages.Rhythms
+{
+    internal static class DrawCommons
+    {
+        public const float RulerOffset = 8;
+
+        public const float NoteBaseSize = 64;
+
+        public static float TimelineNoteUnit { get; internal set; }
+
+        public static float NoteSize { get; internal set; } = 0;
+
+        static public void OutlineBox(Rect rect, float thickness = 1, bool topBorder = false)
+        {
+            Rect outRect = rect;
+
+            if (topBorder)
+            {
+                outRect.height = thickness;
+                GUIUtils.SetColor(Colors.BoxTopColor);
+                GUI.DrawTexture(outRect, Icons.Pixel);
+            }
+
+            GUIUtils.SetColor(Colors.BoxSideColor);
+            outRect.height = rect.height;
+            outRect.width = thickness;
+            GUI.DrawTexture(outRect, Icons.Pixel);
+            outRect.x = rect.xMax - thickness;
+            outRect.height -= thickness;
+            GUI.DrawTexture(outRect, Icons.Pixel);
+
+            outRect = rect;
+            outRect.y = rect.yMax - thickness;
+            outRect.height = thickness;
+            GUIUtils.SetColor(Colors.EnabledHorizontalLine);
+            GUI.DrawTexture(outRect, Icons.Pixel);
+        }
+
+    }
+}
