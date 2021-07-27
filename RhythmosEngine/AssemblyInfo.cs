6aca3f582863b92e66ef0d6d76b8024ad85c23e3
Tobias Ulrich <tobiasbulrich@gmail.com>
2021-07-27 19:59:24 -0300

    Sample: minor update

--- RhythmosEngine/AssemblyInfo.cs
+++ RhythmosEngine/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("RhythmosEngine")]
-[assembly: AssemblyDescription("Rhythmos Engine for Unity3D is an simple library to play and make rhythms.")]
+[assembly: AssemblyDescription("RhythmosEngine for Unity3D is an simple library to play and make rhythms.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
@@ -16,7 +16,7 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.3.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
